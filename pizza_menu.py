acc738c4ca38d343c8196bf09eb5949783bc76f1
Danielle Smith <57499759+Danielle-Smith@users.noreply.github.com>
2019-11-09 20:18:26 -0700

    Add files via upload

---
+++ pizza_menu.py
@@ -0,0 +1,6 @@
+menu = ['pepperoni', 'red pepper', 'tomato', 'cheese']
+
+output = ''
+topping = input('What topping would you like on your pizza? pepperoni, tomato, cheese: ')
+
+print('OK, a', topping, 'pizza comming right up!' )
